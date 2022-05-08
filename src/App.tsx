76695727d16625cfc10b67de804e14bb1bf62b83
IlyaMoroz92 <93884456+IlyaMoroz92@users.noreply.github.com>
2022-05-08 20:10:21 +0300

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button';
+import { ReactComponent as Down} from './components/Button/img/Down.svg' 
+import { ReactComponent as Up} from './components/Button/img/Down.svg' 
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="buttons">
+        <Button 
+          text='Primary'
+          className='primary margin'/>
+        <Button
+          text='Secondary'
+          className='secondary margin'
+        />
+        <Button
+          text='Secondary 2'
+          className='secondary2 margin'
+        />
+        <Button
+          text='Button with icon'
+          icon={<Down />}
+          className='with-icon margin'
+        />
+        <Button
+          icon={<Down/>}
+          className='with-icon-dislike margin'
+          />
+        <Button
+          icon={<Up />}
+          className='with-icon-like margin'
+          />
+      </div>
+      <div className="buttons_disabled">
+      <Button 
+          text='Primary'
+          className='primary margin'
+          disabled
+          />
+        <Button
+          text='Secondary'
+          className='secondary margin'
+          disabled
+        />
+        <Button
+          text='Secondary 2'
+          className='secondary2 margin'
+          disabled
+        />
+        <Button
+          text='Button with icon'
+          icon={<Down />}
+          className='with-icon margin'
+          disabled
+        />
+        <Button
+          icon={<Down/>}
+          className='with-icon-dislike margin'
+          disabled
+          />
+        <Button
+          icon={<Up />}
+          className='with-icon-like margin'
+          disabled
+          />
+      </div>
+      
     </div>
   );
 }
 
-
-
-export default App;
+export default App
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from "react";
+import './Button.scss'
+
+type ButtonProps = {
+    text?: string
+    disabled?: boolean
+    className?: string
+    icon?: ReactElement
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+    alt?: string
+}
+
+export const Button =  (props: ButtonProps) => {
+    return (
+        <button
+        type='button'
+        className={`button button--${props.className}`}
+        disabled={props.disabled}
+        onClick={props.onClick}
+        >
+            {props.icon}
+            {props.text && <span className="button__text">{props.text}</span>}
+        </button>
+    )
+}
