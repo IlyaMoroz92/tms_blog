f6903057ee6896c41ff7c208d2347583d2618f01
IlyaMoroz92 <93884456+IlyaMoroz92@users.noreply.github.com>
2022-05-30 20:51:27 +0300

    feat: add signIn and signUp forms

--- src/pages/SignInPage/signinPage.tsx
+++ src/pages/SignInPage/signinPage.tsx
@@ -1,11 +1,36 @@
 import { Title } from "../../components/Title";
 import { TitleNav } from "../../components/TitleNav";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import {NavLink} from 'react-router-dom'
 
 export const SignInPage = () => {
     return (
         <>
-            <TitleNav  to='/' text='Home'/>
+            <TitleNav  to='/' text='Back to home'/>
             <Title text='Sign In' className='title'/>
+            <div className='sign__form'>
+                <Input
+                    title='Email'
+                    className='password'
+                    type='email'
+                    placeholder='Введите email'
+                />
+                <Input
+                    title='Password'
+                    className='password'
+                    type='password'
+                    placeholder='Введите пароль'
+                    errorMessage='Пароль слишком короткий'
+                />
+                <Button
+                    className='primary sign__button'
+                    type='button'
+                    onClick={()=> console.log('click signIn')}
+                    text='Sign In '
+                />
+                <p className="sign__question">Don't have an account ? <NavLink to='/signup'>Sign Up</NavLink></p>
+            </div>
         </>
     )
 }
--- src/pages/SignUpPage/signupPage.tsx
+++ src/pages/SignUpPage/signupPage.tsx
@@ -1,11 +1,49 @@
 import { Title } from "../../components/Title";
 import { TitleNav } from "../../components/TitleNav";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import {NavLink} from 'react-router-dom'
 
 export const SignUpPage = () => {
     return (
         <>
-            <TitleNav  to='/' text='Home'/>
+            <TitleNav  to='/' text='Back to home'/>
             <Title text='Sign Up' className='title'/>
+            <div className='sign__form'>
+                <Input
+                    title='Name'
+                    className='input'
+                    type='text'
+                    placeholder='Введите имя'
+                />
+                <Input
+                    title='Email'
+                    className='password'
+                    type='email'
+                    placeholder='Введите email'
+                />
+                <Input
+                    title='Password'
+                    className='password'
+                    type='password'
+                    placeholder='Введите пароль'
+                    errorMessage='Пароль слишком короткий'
+                />
+                <Input
+                    title='Confirm password'
+                    className='password'
+                    type='password'
+                    placeholder='Повторите пароль'
+                    errorMessage='Пароль слишком короткий'
+                />
+                <Button
+                    className='primary sign__button'
+                    type='button'
+                    onClick={()=> console.log('click signIn')}
+                    text='Sign Up '
+                />
+                <p className="sign__question">Already have an account ? <NavLink to='/signin'>Sign In</NavLink></p>
+            </div>
         </>
     )
 }
