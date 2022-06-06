f0796ea97dedbdc8ce062ff86fcf6435491e0abf
IlyaMoroz92 <93884456+IlyaMoroz92@users.noreply.github.com>
2022-06-06 16:41:47 +0300

    feat: add url api

--- src/pages/AllPage/allPage.tsx
+++ src/pages/AllPage/allPage.tsx
@@ -1,8 +1,74 @@
 import { Postcard } from "../../components/Postcard";
 import {  content } from '../../components/Postcard/content';
+import { useState, useEffect } from 'react'
 
-export const AllPage = () => {
+
+interface IPost {
+    /* results?: any */
+    id?: number;
+    image?: string;
+    text?: string;
+    date?: any;
+    lesson_num?: number;
+    title?: string;
+    author?: number;
+    like?: boolean;
+    dislike?: boolean;
+    favorit?: boolean;
+    ind?: number
+}
+
+
+export const AllPage: any = () => {
+    const [data, setData] = useState([])
+    useEffect((): any => {
+        const fetchData = async () => {
+            const response = await fetch('https://studapi.teachmeskills.by/blog/posts/?limit=70')
+            const responseFormat = await response.json()
+            setData(responseFormat.results)
+        }
+        fetchData()
+    }, [])
+    console.log(data)
     return (
+        <div className="all__blog">
+            {content.map((el: IPost, ind: number) => {
+                    if(ind < 1) {
+                        return (
+                            <Postcard
+                                key={ind}
+                                title={el.title}
+                                text={el.text}
+                                image={el.image}
+                                date={el.date}
+                                className={`sizeL grid--${el.ind}`}
+                            />
+                        )} else if(ind < 5) {
+                        return (
+                            <Postcard 
+                                key={ind}
+                                title={el.title}
+                                image={el.image}
+                                date={el.date}
+                                className={`sizeM grid--${ind}`}
+                            />
+                        )} else if(ind < 11) {
+                        return (
+                            <Postcard
+                                key={ind}
+                                title={el.title}
+                                image={el.image}
+                                date={el.date}
+                                className={`sizeS grid--${ind}`}
+                            />
+                        )}
+                    }
+                )
+            }
+        </div>
+    );
+    
+    /* return (
         <div className="all__blog">
             {content.map((el, ind) => {
                 if(ind < 1) {
@@ -37,5 +103,5 @@ export const AllPage = () => {
                 }
             )}
         </div>
-    );
+    ); */
 }
